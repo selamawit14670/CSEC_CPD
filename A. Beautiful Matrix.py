48fa0d642e1f8e73b5363bd56845666ec1b254de
selamawit14670 <selamawitd869@gmail.com>
2025-02-12 01:20:04 +0300

    Create A. Beautiful Matrix.py

---
+++ A. Beautiful Matrix.py
@@ -0,0 +1,15 @@
+matrix = [[], [], [], [], []]
+
+for i in range(5):
+    row = list(map(int, input().rstrip().split()))
+    matrix[i] = row 
+location_1 =[]
+for i in range(5):
+    for j in range(5):
+        if matrix[i][j] == 1:
+            location_1 = [i, j]
+middle_x = 2
+middle_y = 2
+
+ans = abs(location_1[0] - middle_x) + abs(location_1[1] -middle_y)
+print(ans)
